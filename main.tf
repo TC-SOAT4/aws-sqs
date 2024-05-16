77803686e3fcdae574c7743735f42a9bd390d324
Thiago <thiagobsn@gmail.com>
2024-05-16 20:59:51 -0300

    Terraform cloud

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "lanchonete-producao-queue.fifo"
-  fifo_queue                  = true
+  fifo_queue                = true
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  cloud {
+    workspaces {
+      name = "lanchonete-terraform-sqs"
+    }
+    organization = "FIAP_POS"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.40.0"
+    }
+
+  }
+
+  required_version = "~> 1.3"
+
+}
