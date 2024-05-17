c1edba8c28f6c835ee4759b61d8535a7b3bdf30a
Thiago <thiagobsn@gmail.com>
2024-05-16 21:06:00 -0300

    Conf regiao aws

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "lanchonete-producao-queue.fifo"
   fifo_queue                = true
