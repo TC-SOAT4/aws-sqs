0e4533bfa6ce50c2152504cac3b67435cb1ce68b
Thiago <thiagobsn@gmail.com>
2024-08-06 21:57:35 -0300

    Ajuste filas

--- main.tf
+++ main.tf
@@ -5,29 +5,29 @@ provider "aws" {
 resource "aws_sqs_queue" "terraform_queue_pagamento" {
   name                      = "lanchonete-pagamento-queue.fifo"
   fifo_queue                = true
-  delay_seconds             = 90
+  delay_seconds             = 0
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 0
 
 }
 
 resource "aws_sqs_queue" "terraform_queue_producao" {
   name                      = "lanchonete-producao-queue.fifo"
   fifo_queue                = true
-  delay_seconds             = 90
+  delay_seconds             = 0
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 0
 
 }
 
 resource "aws_sqs_queue" "terraform_queue_status_pedido" {
   name                      = "lanchonete-pedido-status-queue.fifo"
   fifo_queue                = true
-  delay_seconds             = 90
+  delay_seconds             = 0
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 0
 
 }
